28132e46733697656b5af53b836c260b58a40e27
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2020-04-08 16:33:25 -0700

    added image request and html request to http server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const fs = require("fs");
 
 // defines the url/hostname, and the port
 const hostname = "127.0.0.1";
@@ -7,8 +8,24 @@ const port = process.env.PORT || 3000;
 // creates the server
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("<div className='hello'>Hello World \n</div>");
+  res.setHeader("Content-Type", "image/png");
+
+  fs.readFile("kobyscreen.png", (err, data) => {
+    if (err) throw err;
+    // console.log(data);
+    res.write(data);
+
+    res.end();
+  });
+
+  //   console.log(req);
+
+  //   if (req.url === "/hello-world") {
+  //     setInterval(() => {
+  //       res.write("<div className='hello'>Hello World \n</div>");
+  //     }, 1000);
+  //     return;
+  //   }
 });
 
 // tells the server which port to listen to
