794b952b913feed6d38b79b9d1d3f12224e5273e
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2020-04-07 23:15:41 -0700

    created two servers on two different ports

--- node-practice.js
+++ node-practice.js
@@ -2,12 +2,19 @@ const http = require("http");
 
 const hostname = "127.0.0.1";
 const port = process.env.PORT || 3000;
+const port2 = 4000;
 
 console.log("port CL: ", port);
 
 const options = {
   hostname: hostname,
   port: port,
+  method: "GET",
+};
+
+const options2 = {
+  hostname: hostname,
+  port: port2,
   method: "GET",
 };
 
@@ -18,7 +25,18 @@ const server = http.createServer((req, res) => {
   res.end("Hello World! (end)\n");
 });
 
+const server2 = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.write("Hello there 2.0! (write)\n");
+  res.end("Hello World 2.0! (end)\n");
+});
+
 server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}`);
+});
+
+server2.listen(port2, hostname, () => {
   console.log(`Server running at http://${hostname}`);
 });
 
@@ -38,4 +56,17 @@ req.on("error", (error) => {
   console.error(error);
 });
 
+const req2 = http.request(options2, (res) => {
+  console.log(`statusCode: ${res.statusCode}`);
+
+  res.on("data", (d) => {
+    process.stdout.write(d);
+  });
+});
+
+req2.on("error", (error) => {
+  console.error(error);
+});
+
+req2.end();
 req.end();
